3b44fcdf6ea3ad60a1f2c34d6abc31f16a061f94
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-28 23:50:29 +0600

    created API

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    
+    return (
+        <div className='container mx-auto mt-10'>
+            <h1 className='text-2xl font-medium'>Select Today's Exercise</h1>
+
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className='navbar container mx-auto mt-10'>
+            <a href="/" className='text-4xl font-semibold text-teal-600'>Sports Club</a>
+        </div>
+    );
+};
+
+export default Navbar;

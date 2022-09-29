81adaa6a7c262fbbc6f8fff88d42771778759090
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-29 11:04:06 +0600

    training time functionality added with add to cart

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './ExerciseDetails.css'
 const ExerciseDetails = (props) => {
-    const {} = props.cart;
+    const {cart} = props;
+    let totalTime = 0;
+    for(const activity of cart){
+        totalTime = totalTime + activity.time;
+    }
     return (
         <div>
             <h1 className='text-2xl'>Training Details</h1>
             <div className='flex bg-slate-200 py-5 mt-5 justify-around rounded-lg'>
                 <h3 className='text-xl'>Training time</h3>
-                <p className='text-xl text-gray-500'>mins</p>
+                <p className='text-xl text-gray-500'>{totalTime} mins</p>
             </div>
             <div className='flex bg-slate-200 py-5 mt-5 justify-around rounded-lg'>
                 <h3 className='text-xl'>Break time</h3>

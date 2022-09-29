8db4b77913aa638e54342f5b2b243e5b32a82cd1
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-29 10:52:28 +0600

    exercise details added

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './ExerciseDetails.css'
-const Details = (props) => {
+const ExerciseDetails = (props) => {
     const {} = props.cart;
     return (
         <div>
-            <h1>this is details</h1>
+            <h1 className='text-2xl'>Training Details</h1>
+            <div className='flex bg-slate-200 py-5 mt-5 justify-around rounded-lg'>
+                <h3 className='text-xl'>Training time</h3>
+                <p className='text-xl text-gray-500'>mins</p>
+            </div>
+            <div className='flex bg-slate-200 py-5 mt-5 justify-around rounded-lg'>
+                <h3 className='text-xl'>Break time</h3>
+                <p className='text-xl text-gray-500'>mins</p>
+            </div>
         </div>
     );
 };
 
-export default Details;
+export default ExerciseDetails;
